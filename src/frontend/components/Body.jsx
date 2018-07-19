28079a6d26377fc575911a2e951a8452804651a4
Danddie Porras <danddieporras8@gmail.com>
2018-07-19 14:00:29 +0800

    build: add necessary modules for forms
    add redux-form, redux-logger middleware, and integration of redux-form with semantic-ui

--- src/frontend/components/Body.jsx
+++ src/frontend/components/Body.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import { Route } from 'react-router-dom'
 import App from './App'
 import Inventory from './Inventory'
+import Login from './Login'
 
 const Body = (props) => {
-  // console.log(history)
   return (
     <div>
       <Route exact path='/' component={App} />
       <Route exact path='/Inventory' component={Inventory} />
+      <Route exact path='/login' component={Login} />
     </div>
   )
 }
