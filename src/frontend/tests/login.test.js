c1ecac5911d7fb2d90c2a682352d46d2faa7649a
Danddie Porras <danddieporras8@gmail.com>
2018-08-01 18:03:30 +0800

    test(login-test): modify test for Login component

--- src/frontend/tests/login.test.js
+++ src/frontend/tests/login.test.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import shallow from './enzymeSetup.js'
 import Login from '../components/Login.jsx'
-import { Header, Grid } from 'semantic-ui-react'
+import { Header, Grid, Divider } from 'semantic-ui-react'
 
 describe('<Login />', () => {
   let wrapper
@@ -9,22 +9,25 @@ describe('<Login />', () => {
     wrapper = shallow(<Login />)
   })
   
-  it('should render one <Header /> component', () => {
-    expect(wrapper.find(Header)).toHaveLength(1)
-  })
-  it('should render one <Header.Subheader /> component', () => {
-    expect(wrapper.find(Header.Subheader)).toHaveLength(1)
+  it('should render three <Header /> component', () => {
+    expect(wrapper.find(Header)).toHaveLength(3)
   })
   it('should render one <Grid /> component', () => {
     expect(wrapper.find(Grid)).toHaveLength(1)
   })
-  it('should render one <Grid.Column /> component', () => {
-    expect(wrapper.find(Grid.Column)).toHaveLength(1)
+  it('should render two <Grid.Column /> component', () => {
+    expect(wrapper.find(Grid.Column)).toHaveLength(2)
   })
-  it('should render one <Header.Subheader /> inside the <Header /> component', () => {
-    expect(wrapper.find(Header).dive().find(Header.Subheader)).toHaveLength(1)
+  it('should render one <Grid.Row /> component', () => {
+    expect(wrapper.find(Grid.Row)).toHaveLength(1)
   })
-  it('should render one <Grid.Column /> inside the <Grid /> component', () => {
-    expect(wrapper.find(Grid).dive().find(Grid.Column)).toHaveLength(1)
+  it('should render two <Divider /> component', () => {
+    expect(wrapper.find(Divider)).toHaveLength(1)
+  })
+  it('should render two <Grid.Column /> inside the <Grid.Row /> component', () => {
+    expect(wrapper.find(Grid.Row).dive().find(Grid.Column)).toHaveLength(2)
+  })
+  it('should render one <Grid.Row /> inside the <Grid /> component', () => {
+    expect(wrapper.find(Grid).dive().find(Grid.Row)).toHaveLength(1)
   })
 })
