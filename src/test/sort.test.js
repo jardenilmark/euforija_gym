514bd8b2b71915a5f7a31b5977f535d8c6ec51fe
Mark Jardenil <jardenilmark@gmail.com>
2018-08-04 18:25:06 +0800

    test: fix test

--- src/test/sort.test.js
+++ src/test/sort.test.js
@@ -11,6 +11,9 @@ describe('<Sort />', () => {
       age: 19
     }]
   })
+  it('gets arr[0]', () => {
+    expect(arr[0].name).toBe('mark')
+  })
   it('sorts the array by name', () => {
     compareData(arr, 'name')
     expect(arr[0].name).toBe('danddie')
