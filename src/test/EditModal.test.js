38db6ba46245fcaddb58ed919cbe9043b1d84f1b
Danddie Porras <danddieporras8@gmail.com>
2018-08-09 00:56:26 +0800

    test: fix tests for modals

--- src/test/EditModal.test.js
+++ src/test/EditModal.test.js
@@ -2,24 +2,24 @@ import React from 'react'
 import shallow from './Enzyme.js'
 import EditModal from '../frontend/components/inventory/EditModal'
 import EditForm from '../frontend/redux/containers/inventory/EditFormContainer'
-import { Modal, Container } from 'semantic-ui-react'
+import { Modal, Container, Header } from 'semantic-ui-react'
 
-describe('<AddModal />', () => {
+describe('<EditModal />', () => {
   let wrapper
   beforeEach(() => {
     wrapper = shallow(<EditModal />)
   })
-  it('must contain a modal', () => {
+  it('must contain a Modal', () => {
     expect(wrapper.find(Modal)).toHaveLength(1)
   })
-  it('must contain a container', () => {
-    expect(wrapper.find(Container).length).toBeGreaterThanOrEqual(1)
+  it('must contain a Container', () => {
+    expect(wrapper.find(Container)).toHaveLength(1)
   })
-  it('first container must have text align right', () => {
-    expect(wrapper.find(Container).at(0).props().textAlign).toBe('right')
+  it('Container must have text align center', () => {
+    expect(wrapper.find(Container).props().textAlign).toBe('center')
   })
-  it('second container must have text align middle', () => {
-    expect(wrapper.find(Container).at(1).props().textAlign).toBe('center')
+  it('must contain a Header', () => {
+    expect(wrapper.find(Header)).toHaveLength(1)
   })
   it('must contain a form that edits items', () => {
     expect(wrapper.find(EditForm)).toHaveLength(1)
