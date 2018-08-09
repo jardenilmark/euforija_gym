a1fa811a7428f8e7c3960c787a401ab0af2c13e0
Nica Marielle Dela Cruz <niczdelacruz25@gmail.com>
2018-08-09 11:33:25 +0800

    style: fix linter errors and change file structure

---
+++ src/backend/index.js
@@ -0,0 +1,10 @@
+import createServer from '../createServer'
+
+const startServer = async () => {
+  const app = await createServer()
+  app.listen(app.get('port')).on('listening', () =>
+    console.log('Server listening on localhost:3000')
+  )
+}
+
+startServer()
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
-const path = require('path');
-const webpack = require('webpack');
+const path = require('path')
+const webpack = require('webpack')
 
 const options = {
   cache: true,
@@ -8,35 +8,49 @@ const options = {
     core: [
       'babel-polyfill',
       path.join(__dirname, 'src/frontend/index.jsx'),
-    ],
+    ]
   },
 
   output: {
     filename: 'bundle.js',
-    path: path.join(process.cwd(), 'public'),
-    publicPath: '/js',
+    path: path.join(process.cwd(), 'public/js'),
+    publicPath: '/js'
   },
 
   module: {
-    rules: [{
-      test: /\.jsx?$/,
-      exclude: /node_modules/,
-      use: [{
+    rules: [
+      {
+        test: /\.(js|jsx)?$/,
+        exclude: /node_modules/,
         loader: 'babel-loader',
-      }],
-    }],
+        query: {
+          presets: ['env', 'react']
+        }
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+        loader: 'url-loader?limit=100000'
+      },
+      {
+        test: /\.css$/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader' }
+        ]
+      }]
   },
 
   resolve: {
-    extensions: ['.js', '.jsx'],
+    extensions: ['.js', '.jsx']
   },
 
   plugins: [],
-
-};
+  mode: 'development',
+  performance: { hints: false }
+}
 
 if (process.env.NODE_ENV === 'production') {
   options.plugins.push(new webpack.optimize.UglifyJsPlugin());
 }
 
-module.exports = options;
+module.exports = options
