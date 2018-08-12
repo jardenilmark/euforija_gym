fb22d135ffd9c1412c36b2ae10bda5812bf5051d
Nica Marielle Dela Cruz <niczdelacruz25@gmail.com>
2018-08-12 20:36:07 +0800

    refactor: changed serviceIndex to setupAllServices

--- src/backend/services/setupAllServices.js
+++ src/backend/services/setupAllServices.js
@@ -1,11 +1,13 @@
 import setupStudentService from './studentService'
 import setupStaffService from './staffService'
+import setupInventoryService from './inventoryService'
 
 const setupAllServices = (app, db) => {
   return () => {
     app
       .configure(setupStaffService(app, db))
       .configure(setupStudentService(app, db))
+      .configure(setupInventoryService(app, db))
   }
 }
 
