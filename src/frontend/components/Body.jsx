06375a607f18242535f4dee2237119b8c0bd679c
Lennar de la Puerta <iamlennar@gmail.com>
2018-08-30 16:06:27 +0800

    feat: add attendance route

--- src/frontend/components/Body.jsx
+++ src/frontend/components/Body.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { Route } from 'react-router-dom'
 import App from './App'
 import Inventory from '../redux/containers/inventory/InventoryContainer'
+import Attendance from '../components/attendance/Attendance'
 
 const Body = () => {
   return (
     <div>
       <Route exact path='/' component={App} />
       <Route exact path='/Inventory' component={Inventory} />
+      <Route exact path='/Attendance' component={Attendance} />
     </div>
   )
 }
