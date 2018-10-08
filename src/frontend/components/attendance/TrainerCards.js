e67d2edad72051e991174aa9cd813310922579e0
Lennar de la Puerta <iamlennar@gmail.com>
2018-10-09 01:41:43 +0800

    feat: add cards for trainor attendance

---
+++ src/frontend/components/attendance/TrainerCards.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Card, Image } from 'semantic-ui-react'
+import { TrainerName, CardsContainer } from './AttendanceStyled'
+
+const TrainerCards = ({ trainers }) => (
+  <CardsContainer>
+    <Card.Group itemsPerRow={5} >
+
+      {trainers.map(trainer => (
+        <Card key={trainer._id}>
+          <Image src='https://react.semantic-ui.com/images/avatar/large/matthew.png' />
+          <TrainerName>
+            {trainer.firstName}
+          </TrainerName>
+        </Card>
+      ))}
+    </Card.Group>
+  </CardsContainer>
+)
+
+export default TrainerCards
