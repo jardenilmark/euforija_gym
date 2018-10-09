794770062d839dc26b22f7659d47a41796af054b
Lennar de la Puerta <iamlennar@gmail.com>
2018-10-09 13:58:42 +0800

    feat: add attendance service

---
+++ src/backend/services/attendanceService.js
@@ -0,0 +1,9 @@
+import createService from './createService'
+
+const attendanceService = (app, db) => {
+  return () => {
+    createService(app, db, 'attendance')
+  }
+}
+
+export default attendanceService
