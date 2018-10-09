ec8a3b617623d8879e894a090658350ad8c1327e
Danddie Porras <danddieporras8@gmail.com>
2018-10-09 09:51:57 +0800

    chore: replace history.push with window.location.assign

--- src/frontend/redux/actions/loginActions.js
+++ src/frontend/redux/actions/loginActions.js
@@ -1,9 +1,9 @@
 import app from '../../client'
 import swal from 'sweetalert'
-import createHistory from 'history/createBrowserHistory'
+// import createHistory from 'history/createBrowserHistory'
 
 const location = 'api/staff'
-const history = createHistory()
+// const history = createHistory()
 
 export function handleLogin (data) {
   return async (dispatch) => {
@@ -13,8 +13,8 @@ export function handleLogin (data) {
     if (result.length > 0) {
       const user = result[0]
       if (user.password === data.password) {
+        window.location.assign('/')
         swal('Success', 'You have successfully logged in.', 'success')
-        history.push('/')
       } else {
         swal('Oops', 'The password you entered is incorrect.', 'error')
       }
