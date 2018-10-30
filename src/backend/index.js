51ad11b0deaa0ae6abd0ed295ff5d4f885457c80
Mark Jardenil <jardenilmark@gmail.com>
2018-10-30 16:47:00 +0800

    feat: add concurrecntly
    no more need of multiple consoles

--- src/backend/index.js
+++ src/backend/index.js
@@ -4,7 +4,7 @@ const startServer = async () => {
 	const app = await createServer()
 	app
 		.listen(app.get('port'))
-		.on('listening', () => console.log('Server listening on http://localhost:3000'))
+		.on('listening', () => console.log(`Server listening on http://localhost:${app.get('port')}`))
 }
 
 startServer()
