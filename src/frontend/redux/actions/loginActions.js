66cd622dc1891a95c55c926447cf25b81d1a71e1
Danddie Porras <danddieporras8@gmail.com>
2019-01-16 21:16:27 +0800

    refactor: remove unnecessary dispatch parameter on async function

--- src/frontend/redux/actions/loginActions.js
+++ src/frontend/redux/actions/loginActions.js
@@ -6,20 +6,18 @@ const location = 'api/staff'
 // const history = createHistory()
 
 export function handleLogin(data) {
-	return async dispatch => {
-		const result = await app.service(location).find({
-			query: { idNumber: data.idNumber }
-		})
-		if (result.length > 0) {
-			const user = result[0]
-			if (user.password === data.password) {
-				window.location.assign('/')
-				swal('Success', 'You have successfully logged in.', 'success')
-			} else {
-				swal('Oops', 'The password you entered is incorrect.', 'error')
-			}
+	const result = await app.service(location).find({
+		query: { idNumber: data.idNumber }
+	})
+	if (result.length > 0) {
+		const user = result[0]
+		if (user.password === data.password) {
+			window.location.assign('/')
+			swal('Success', 'You have successfully logged in.', 'success')
 		} else {
-			swal('Oops', 'User not found.', 'error')
+			swal('Oops', 'The password you entered is incorrect.', 'error')
 		}
+	} else {
+		swal('Oops', 'User not found.', 'error')
 	}
 }
