5143909990e4d82911e9336cecd4a6fe9b4c1418
Danddie Porras <danddieporras8@gmail.com>
2019-02-09 14:40:42 +0800

    feat: show image captured when capture button is clicked
    add the ability to retry when image captured is not ideal

--- src/frontend/components/staff/ProfilePhoto.jsx
+++ src/frontend/components/staff/ProfilePhoto.jsx
@@ -1,5 +1,5 @@
 import Webcam from 'react-webcam'
-import { Button } from 'semantic-ui-react'
+import { Button, Image } from 'semantic-ui-react'
 import React from 'react'
 
 class ProfilePhoto extends React.Component {
@@ -9,29 +9,55 @@ class ProfilePhoto extends React.Component {
 
 	capture = () => {
 		const img = this.webcam.getScreenshot()
+		this.props.saveImage(img)
 	}
 
 	render() {
+		const hasCaptured = this.props.image === ''
 		return (
 			<div style={{ textAlign: 'center' }}>
-				<Webcam
-					height={300}
-					width={300}
-					ref={this.setRef}
-					imageSmoothing={true}
-					style={{ marginBottom: -20, marginTop: -25 }}
-					screenshotFormat={'image/jpeg'}
-				/>
-				<div>
-					<Button
-						content={'Capture Image'}
-						type={'button'}
-						onClick={this.capture}
-						icon={'camera retro'}
+				{hasCaptured ? (
+					<Webcam
+						height={350}
+						width={300}
+						ref={this.setRef}
+						imageSmoothing={true}
+						style={styles.webcam}
+						screenshotFormat={'image/jpeg'}
 					/>
+				) : (
+					<Image src={this.props.image} centered style={styles.image} />
+				)}
+				<div>
+					{hasCaptured ? (
+						<Button
+							content={'Capture Image'}
+							type={'button'}
+							onClick={this.capture}
+							icon={'camera retro'}
+						/>
+					) : (
+						<Button
+							content={'Retry'}
+							type={'button'}
+							onClick={this.props.clearImage}
+							icon={'sync alternate'}
+						/>
+					)}
 				</div>
 			</div>
 		)
+	}
+}
+
+const styles = {
+	webcam: {
+		marginBottom: -15,
+		marginTop: -50
+	},
+	image: {
+		marginBottom: 40,
+		marginTop: 50
 	}
 }
 
