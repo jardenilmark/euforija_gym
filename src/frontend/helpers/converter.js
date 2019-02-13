67624698ce5b5325b159ac6282b37c7f1ce4c0d0
Danddie Porras <danddieporras8@gmail.com>
2019-02-13 19:48:23 +0800

    fix: fix bug when editing inventory item and image field is not pre-filled

--- src/frontend/helpers/converter.js
+++ src/frontend/helpers/converter.js
@@ -1,4 +1,5 @@
 const getBase64 = file => {
+	console.log('FILE', file)
 	return new Promise((resolve, reject) => {
 		const reader = new FileReader()
 		reader.readAsDataURL(file)
@@ -7,9 +8,38 @@ const getBase64 = file => {
 	})
 }
 
+function getFile(base64String) {
+	const base64Image = base64String
+	const arr = base64Image.split(';')
+	const contentType = arr[0].split(':')[1]
+	const imageData = arr[1].split(',')[1]
+
+	const sliceSize = 512
+
+	const byteCharacters = atob(imageData)
+	let byteArrays = []
+
+	for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+		const slice = byteCharacters.slice(offset, offset + sliceSize)
+
+		const byteNumbers = new Array(slice.length)
+		for (let i = 0; i < slice.length; i++) {
+			byteNumbers[i] = slice.charCodeAt(i)
+		}
+
+		const byteArray = new Uint8Array(byteNumbers)
+
+		byteArrays.push(byteArray)
+	}
+
+	const blob = new Blob(byteArrays, { type: contentType })
+	const file = new File([blob], 'image', { lastModified: new Date() })
+	return file
+}
+
 const converter = async targetFile => {
 	const data = await getBase64(targetFile)
 	return data
 }
 
-export { converter }
+export { converter, getFile }
