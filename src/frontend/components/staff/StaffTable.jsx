4546bfd6689961624415a7b165bd6f3d15a69df0
Danddie Porras <danddieporras8@gmail.com>
2019-02-14 16:56:55 +0800

    feat: show placeholder segment when no staff exists in the database

--- src/frontend/components/staff/StaffTable.jsx
+++ src/frontend/components/staff/StaffTable.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import TableRow from './TableRow'
-import { Table, Loader, Segment } from 'semantic-ui-react'
+import { Table, Loader, Segment, Header, Icon } from 'semantic-ui-react'
 
 class StaffTable extends React.Component {
 	componentDidMount() {
@@ -9,7 +9,6 @@ class StaffTable extends React.Component {
 
 	render() {
 		let rows
-		console.log(this.props)
 		if (this.props.isFetching) {
 			rows = (
 				<Table.Row>
@@ -21,17 +20,33 @@ class StaffTable extends React.Component {
 				</Table.Row>
 			)
 		} else {
-			rows = this.props.staff.map((staff, index) => {
-				return (
-					<TableRow
-						staff={staff}
-						key={index}
-						toggleProfileVisibility={this.props.toggleProfileVisibility}
-						toggleFormVisibility={this.props.toggleFormVisibility}
-						setClickedStaff={this.props.setClickedStaff}
-					/>
+			if (this.props.staff.length > 0) {
+				rows = this.props.staff.map((staff, index) => {
+					return (
+						<TableRow
+							staff={staff}
+							key={index}
+							toggleProfileVisibility={this.props.toggleProfileVisibility}
+							toggleFormVisibility={this.props.toggleFormVisibility}
+							setClickedStaff={this.props.setClickedStaff}
+						/>
+					)
+				})
+			} else {
+				rows = (
+					<Table.Row>
+						<Table.Cell>
+							<Segment placeholder style={{ border: 'dotted 5px' }} size={'big'}>
+								<Header icon>
+									<Icon name="exclamation circle" color={'red'} />
+									<Header.Content>No staff exists in the database.</Header.Content>
+									<Header.Content>Click the button above to add one.</Header.Content>
+								</Header>
+							</Segment>
+						</Table.Cell>
+					</Table.Row>
 				)
-			})
+			}
 		}
 
 		return (
