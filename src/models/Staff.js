d5e87ae998a246b4db4ad42a42cc7f2eb5778a0e
Nica Marielle Dela Cruz <niczdelacruz25@gmail.com>
2019-02-19 00:44:39 +0800

    test: Added models and tests for Staff and Student

---
+++ src/models/Staff.js
@@ -0,0 +1,22 @@
+import Model from './Model'
+import Joi from 'joi'
+
+class Staff extends Model {
+  static get schema() {
+    return {
+      idNumber: Joi.string().alphanum().min(8).max(8).required(),
+      firstName: Joi.string().regex(/^[a-zA-Z ]*$/).required(),
+      lastName: Joi.string().regex(/^[a-zA-Z ]*$/).required(),
+      gender: Joi.any().valid(['male', 'female']).required(),
+      birthdate: Joi.date().required(),
+      contactNumber: Joi.string().regex(/[0-9]*$/).max(11).required(),
+      address: Joi.string().required(),
+      role: Joi.any().valid(['Coach', 'Maintenance']).required(),
+      password: Joi.string().alphanum().min(8).required(),
+      // image: faker.image.avatar(),
+      status: Joi.any().valid(['in', 'out']).required()
+    }
+  }
+}
+
+export default Staff
