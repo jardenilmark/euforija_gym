87d89c4d920f8d05acb9c3bd38c018e46dd00a50
Nica Marielle Dela Cruz <niczdelacruz25@gmail.com>
2019-03-07 18:52:13 +0800

    test: added test for inventory service

---
+++ src/test/inventoryService.test.js
@@ -0,0 +1,36 @@
+import faker from 'faker'
+
+describe('inventoryService', () => {
+  describe('#setupInventoryService', () => {
+    const findStub = jest.fn()
+    const createStub = jest.fn()
+    const serviceStub = jest.fn()
+    let fakeApp = jest.fn()
+    let item = {
+      name: faker.commerce.productMaterial,
+      quantity: faker.random.number,
+      unit: 'pieces',
+      price: faker.commerce.price
+    }
+
+    fakeApp = {
+      service: serviceStub
+    }
+
+    serviceStub.mockReturnValue({
+      create: createStub,
+      find: findStub,
+      update: updateStub,
+    })
+
+    test('creates a new inventory item', () => {
+      createStub.mockResolvedValue(item)
+      expect(fakeApp.service('inventoryService').create(item)).not.toEqual('{}')
+    })
+
+    test('finds all items in the inventory', () => {
+      findStub.mockResolvedValue(item)
+      expect(fakeApp.service('inventoryService').find()).toEqual(Promise.resolve(item))
+    })
+  })
+})
