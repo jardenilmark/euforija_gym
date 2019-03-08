34b416af359ccff933381dc6916c3ab2775bba26
Danddie Porras <danddieporras8@gmail.com>
2019-03-08 20:55:58 +0800

    refactor: change path of routes to sync with the homescreen routes

--- src/frontend/components/Body.jsx
+++ src/frontend/components/Body.jsx
@@ -5,8 +5,7 @@ import Attendance from '../components/attendance/Attendance'
 import HomeScreen from '../redux/containers/homescreen/HomeScreenContainer'
 import Sale from '../redux/containers/sale/SaleContainer'
 import Login from '../redux/containers/login/LoginContainer'
-import Student from './student/StudentPage'
-import CreateStudent from '../redux/containers/student/CreateStudentPage'
+import Student from '../redux/containers/student/StudentPage'
 import Staff from '../redux/containers/staff/StaffPage'
 import Report from '../redux/containers/report/IncomeReport'
 import Payroll from '../components/payroll'
@@ -24,7 +23,6 @@ const Body = () => {
 			<Route exact path={'/attendance'} component={Attendance} />
 			<Route exact path={'/staff'} component={Staff} />
 			<Route exact path={'/student'} component={Student} />
-			<Route exact path={'/create-student'} component={CreateStudent} />
 			<Route exact path={'/income-report'} component={Report} />
 			<Route exact path={'/payroll'} component={Payroll} />
 		</div>
