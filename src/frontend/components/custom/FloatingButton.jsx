b0e559702b63c94aa89edee87a4ed495cbe0ebd2
Danddie Porras <danddieporras8@gmail.com>
2019-03-25 21:23:29 +0800

    refactor: modify temporary fab nav buttons

--- src/frontend/components/custom/FloatingButton.jsx
+++ src/frontend/components/custom/FloatingButton.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Fab, Action } from 'react-tiny-fab'
-import { Icon, Button } from 'semantic-ui-react'
 import 'react-tiny-fab/dist/styles.css'
 
 const FloatingButton = () => {
@@ -21,39 +20,59 @@ const FloatingButton = () => {
 				H
 			</Action>
 			<Action
-				text="Inventory"
-				style={{ backgroundColor: 'black' }}
-				// onClick={handleHelpOnClick}
-			>
-				{/* <i className="fa fa-help" /> */}
-			</Action>
-			<Action
 				text="Sales"
-				style={{ backgroundColor: 'black' }}
-				// onClick={handleHelpOnClick}
-			>
-				{/* <i className="fa fa-help" /> */}
+				style={{
+					backgroundColor: 'transparent',
+					fontSize: '20',
+					fontFamily: 'Lane',
+					color: 'black'
+				}}
+				onClick={() => window.location.assign('/sales')}>
+				S
 			</Action>
 			<Action
 				text="Staff"
-				style={{ backgroundColor: 'black' }}
-				// onClick={handleHelpOnClick}
-			>
-				{/* <i className="fa fa-help" /> */}
+				style={{
+					backgroundColor: 'transparent',
+					fontSize: '20',
+					fontFamily: 'Lane',
+					color: 'black'
+				}}
+				onClick={() => window.location.assign('/staff')}>
+				S
+			</Action>
+			<Action
+				text="Income Reports"
+				style={{
+					backgroundColor: 'transparent',
+					fontSize: '20',
+					fontFamily: 'Lane',
+					color: 'black'
+				}}
+				onClick={() => window.location.assign('/income-report')}>
+				I
 			</Action>
 			<Action
 				text="Student"
-				style={{ backgroundColor: 'black' }}
-				// onClick={handleHelpOnClick}
-			>
-				{/* <i className="fa fa-help" /> */}
+				style={{
+					backgroundColor: 'transparent',
+					fontSize: '20',
+					fontFamily: 'Lane',
+					color: 'black'
+				}}
+				onClick={() => window.location.assign('/student')}>
+				S
 			</Action>
 			<Action
 				text="Payroll"
-				style={{ backgroundColor: 'black' }}
-				// onClick={handleHelpOnClick}
-			>
-				{/* <i className="fa fa-help" /> */}
+				style={{
+					backgroundColor: 'transparent',
+					fontSize: '20',
+					fontFamily: 'Lane',
+					color: 'black'
+				}}
+				onClick={() => window.location.assign('/payroll')}>
+				P
 			</Action>
 		</Fab>
 	)
