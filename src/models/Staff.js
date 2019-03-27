40919ceb631bddb1ca2ff17579fe78101049031b
Nica Marielle Dela Cruz <niczdelacruz25@gmail.com>
2019-03-27 23:19:04 +0800

    fix: changed model schema

--- src/models/Staff.js
+++ src/models/Staff.js
@@ -2,21 +2,29 @@ import Model from './Model'
 import Joi from 'joi'
 
 class Staff extends Model {
-  static get schema() {
-    return {
-      idNumber: Joi.string().alphanum().min(8).max(8).required(),
-      firstName: Joi.string().regex(/^[a-zA-Z ]*$/).required(),
-      lastName: Joi.string().regex(/^[a-zA-Z ]*$/).required(),
-      gender: Joi.any().valid(['male', 'female']).required(),
-      birthdate: Joi.date().required(),
-      contactNumber: Joi.string().regex(/[0-9]*$/).max(11).required(),
-      address: Joi.string().required(),
-      role: Joi.any().valid(['Coach', 'Maintenance']).required(),
-      password: Joi.string().alphanum().min(8).required(),
-      image: Joi.string().base64().required(),
-      status: Joi.any().valid(['in', 'out']).required()
-    }
-  }
+	static get schema() {
+		return {
+			idNumber: Joi.string()
+				.min(8)
+				.required(),
+			firstName: Joi.string().required(),
+			lastName: Joi.string().required(),
+			gender: Joi.any()
+				.valid(['male', 'female'])
+				.required(),
+			birthdate: Joi.string().required(),
+			contactNumber: Joi.string().required(),
+			address: Joi.string().required(),
+			role: Joi.any()
+				.valid(['Coach', 'Maintenance', 'Owner'])
+				.required(),
+			password: Joi.string().required(),
+			image: Joi.string().required(),
+			status: Joi.any()
+				.valid(['in', 'out'])
+				.required()
+		}
+	}
 }
 
 export default Staff
--- src/models/Student.js
+++ src/models/Student.js
@@ -2,19 +2,22 @@ import Model from './Model'
 import Joi from 'joi'
 
 class Student extends Model {
-  static get schema () {
-    return {
-      idNumber: Joi.string().alphanum().min(8).max(8).required(),
-			firstName: Joi.string().regex(/^[a-zA-Z ]*$/).required(),
-			lastName: Joi.string().regex(/^[a-zA-Z ]*$/).required(),
-			gender: Joi.any().valid(['male', 'female']).required(),
-			birthdate: Joi.date().required(),
-      contactNumber: Joi.string().regex(/[0-9]*$/).max(11).required(),
+	static get schema() {
+		return {
+			idNumber: Joi.string()
+				.min(8)
+				.required(),
+			firstName: Joi.string().required(),
+			lastName: Joi.string().required(),
+			gender: Joi.any()
+				.valid(['male', 'female'])
+				.required(),
+			birthdate: Joi.string().required(),
+			contactNumber: Joi.string().required(),
 			address: Joi.string().required(),
-      password: Joi.string().alphanum().min(8).required(),
-			image: Joi.string().base64().required(),
-    }
-  }
+			image: Joi.string().required()
+		}
+	}
 }
 
 export default Student
