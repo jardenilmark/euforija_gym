7e9c44be1d7737724b52b39f672e182a06efca8f
Nica Marielle Dela Cruz <niczdelacruz25@gmail.com>
2019-03-27 15:20:22 +0800

    fix: revert password encryption

--- src/backend/services/staffService.js
+++ src/backend/services/staffService.js
@@ -1,31 +1,17 @@
 import createServices from './createService'
-import { hooks } from '@feathersjs/authentication-local'
 import transformHook from '../hooks/transfrom'
 import validate from '../hooks/validate'
 import Staff from '../../models/Staff'
+import encryptPassword from '../hooks/user'
 
 const setupStaffService = (app, db) => {
 	return () => {
 		const service = createServices(app, db, 'staff')
 		service.hooks({
 			before: {
-				create: [
-					transformHook(Staff),
-					validate(),
-					hooks.hashPassword({
-						passwordField: 'password'
-					})
-				],
-				update: [
-					hooks.hashPassword({
-						passwordField: 'password'
-					})
-				],
-				patch: [
-					hooks.hashPassword({
-						passwordField: 'password'
-					})
-				]
+				create: [transformHook(Staff), validate(), encryptPassword()],
+				update: [encryptPassword()],
+				patch: [encryptPassword()]
 			}
 		})
 	}
